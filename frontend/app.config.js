bcc9471c9d7389d021b51278e7901f596f5cab82
K1C <kc212025@gmail.com>
2025-01-17 13:03:17 -0500

    working

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,8 +1,11 @@
-// frontend/app.config.js
+import 'dotenv/config';
 
 export default ({ config }) => ({
   ...config,
   extra: {
-    apiBaseUrl: 'http://10.0.0.203:5000', // Use development machine's IP address
+    apiBaseUrl: process.env.API_BASE_URL || 'http://10.0.0.203:5000',
+  },
+  experiments: {
+    tsconfigPaths: true,
   },
 });
