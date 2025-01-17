c4f80d86037c023e12cd387704a4ef1a041680d9
K1C <kc212025@gmail.com>
2025-01-17 17:07:34 -0500

    working

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,8 +1,15 @@
 // frontend/app.config.js
 
 export default ({ config }) => ({
-  ...config,
+  ...config, 
   extra: {
-    apiBaseUrl: 'http://10.0.0.203:5000', // Use development machine's IP address
+    apiBaseUrl: 'http://10.0.0.203:5000', // Your development machine's IP address
   },
+  'react-native-google-mobile-ads': {
+    android_app_id: 'ca-app-pub-2191904332416469~4553503462',
+    ios_app_id: 'ca-app-pub-xxxxxxxx~xxxxxxxx',
+  },
+  plugins: [
+    "react-native-google-mobile-ads" 
+  ],
 });
