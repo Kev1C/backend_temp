9e3548501973199e01238025e65f6b2a0fa597b9
K1C <kc212025@gmail.com>
2025-01-18 23:47:32 -0500

    test

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -5,11 +5,11 @@ export default ({ config }) => ({
   extra: {
     apiBaseUrl: 'http://10.0.0.203:5000', // Your development machine's IP address
   },
-  'react-native-google-mobile-ads': {
-    android_app_id: 'ca-app-pub-2191904332416469~4553503462',
-    ios_app_id: 'ca-app-pub-xxxxxxxx~xxxxxxxx',
-  },
-  plugins: [
-    "react-native-google-mobile-ads" 
-  ],
+  // 'react-native-google-mobile-ads': {
+  //   android_app_id: 'ca-app-pub-2191904332416469~4553503462',
+  //   ios_app_id: 'ca-app-pub-xxxxxxxx~xxxxxxxx',
+  // },
+  // plugins: [
+  //   "react-native-google-mobile-ads" 
+  // ],
 });
