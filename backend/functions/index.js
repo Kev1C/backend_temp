4cbb44f77085e52d3cd4ef10635e4da6d88f7262
K1C <kc212025@gmail.com>
2025-03-03 15:16:02 -0500

    package and node 20

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,19 +1,19 @@
 //backend/functions/index.js
+const functions = require("firebase-functions");
 const {onRequest} = require("firebase-functions/v2/https");
 const admin = require("firebase-admin");
 
-// Initialize Firebase Admin only once
 if (!admin.apps.length) {
   admin.initializeApp();
   console.log("Firebase Admin initialized in index.js");
 }
 const app = require("./app");
-// Export your Express app as a Cloud Function named 'api' using v2 syntax
 exports.api = onRequest(
     {
       timeoutSeconds: 540,
       memory: "1GB",
-      region: "us-central1", // Explicitly specify a region
+      region: "us-central1",
+      minInstances: 0,
     },
     app,
 );
