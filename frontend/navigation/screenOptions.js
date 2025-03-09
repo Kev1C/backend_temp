95094082ad8a35734cb4bfc2b56898c7b5d7585a
Kev1C <54962767+Kev1C@users.noreply.github.com>
2025-03-08 22:53:05 -0500

    Delete frontend directory

--- frontend/navigation/screenOptions.js
+++
@@ -1,5 +0,0 @@
-export const defaultScreenOptions = {
-    headerStyle: { backgroundColor: '#4F8EF7' },
-    headerTintColor: '#fff',
-    headerTitleStyle: { fontWeight: 'bold' },
-  };
--- frontend/screens/Home/MemoizedComponents.js
+++
@@ -1,9 +0,0 @@
-//frontend/screens/Home/MemoizedComponents.js
-import React, { memo } from 'react';
-import WeekCalendar from '../../Components/WeekCalendar';
-import CalorieProgress from '../../Components/CalorieProgress';
-import RecentlyEaten from '../../Components/RecentlyEaten';
-
-export const MemoizedWeekCalendar = memo(WeekCalendar);
-export const MemoizedCalorieProgress = memo(CalorieProgress);
-export const MemoizedRecentlyEaten = memo(RecentlyEaten);
